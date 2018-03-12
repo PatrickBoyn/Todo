d24f0e6c0aecb7039c35dba14cb69c2e123c32f0
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-12 00:46:54 -0400

    Added the hover effect for all li's. I am starting to get the hang of this.

--- Todo.js
+++ Todo.js
@@ -1,9 +1,13 @@
 var li = document.querySelectorAll("li");
 
-li.addEventListener("mouseover", function(){
-    li.style.color = "green";
-});
+for(var i = 0; i < li.length; i++){
+    li[i].addEventListener("mouseover", function(){
+        this.style.color = "green";
+    });
+}
 
-li.addEventListener("mouseout", function(){
-    li.style.color = "black";
-});
+for(var i = 0; i < li.length; i++){
+    li[i].addEventListener("mouseout", function(){
+        this.style.color = "black";
+    });
+}
